ae6fb49470951b7dfcc40e01a9c58cf6d86a8950
Matteo Chauvin <mchauvin@z2r6p5.42lyon.fr>
2026-04-04 13:15:05 +0200

    fix(python06): updated code to match new subject

--- python06/alchemy/potions.py
+++ python06/alchemy/potions.py
@@ -1,16 +1,12 @@
-from alchemy import elements
+from alchemy.elements import create_earth, create_air
+from elements import create_fire, create_water
 
 
 def healing_potion() -> str:
-    return f"Healing potion brewed with {elements.create_fire()}" \
-        f"and {elements.create_water()}"
+    return f"Healing potion brewed with {create_earth()}" \
+        f"and {create_air()}"
 
 
 def strength_potion() -> str:
-    return f"Strength potion brewed with {elements.create_earth()}" \
-        f"and {elements.create_fire()}"
-
-
-def invisibility_potion() -> str:
-    return f"Invisibility potion brewed with {elements.create_air()}" \
-        f"and {elements.create_water()}"
+    return f"Strength potion brewed with {create_fire()}" \
+        f"and {create_water()}"
--- python06/alchemy/transmutation/__init__.py
+++ python06/alchemy/transmutation/__init__.py
@@ -1,2 +1,1 @@
-from .basic import lead_to_gold, stone_to_gem # noqa
-from .advanced import philosophers_stone, elixir_of_life # noqa
+from .recipes import lead_to_gold # noqa
