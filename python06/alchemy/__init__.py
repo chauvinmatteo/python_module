8ac6c3d115ee799dddc0c8acd08caa6c14145407
Matteo Chauvin <229449054+chauvinmatteo@users.noreply.github.com>
2026-04-23 12:13:29 +0200

    dev(06): finished module6 - 100/100

--- python06/alchemy/__init__.py
+++ python06/alchemy/__init__.py
@@ -1,4 +1,12 @@
-from .elements import create_air # noqa
-from .potions import healing_potion as heal # noqa
-from .potions import strength_potion # noqa
-from .transmutation.recipes import lead_to_gold # noqa
+from .elements import create_air
+from .potions import healing_potion as heal
+from .potions import strength_potion
+from .transmutation.recipes import lead_to_gold
+
+
+__all__ = [
+    "heal",
+    "create_air",
+    "strength_potion",
+    "lead_to_gold"
+]
--- python06/alchemy/grimoire/__init__.py
+++ python06/alchemy/grimoire/__init__.py
@@ -1,1 +1,5 @@
-from .light_spellbook import light_spell_record, light_spell_allowed_ingredients # noqa
+from .light_spellbook import (light_spell_record,
+                              light_spell_allowed_ingredients)
+
+
+__all__ = ["light_spell_record", "light_spell_allowed_ingredients"]
