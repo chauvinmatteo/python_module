bd1f5553ffa0324447a4ff13c095e7ccad87942a
Matteo Chauvin <229449054+chauvinmatteo@users.noreply.github.com>
2026-04-23 12:13:28 +0200

    dev(ex0): finished ex0 - mypy wip

--- python07/battle.py
+++ python07/battle.py
@@ -1,13 +1,38 @@
 import ex0
 
 
-def firetype_factory() -> None:
+def test_factory(factory):
     print("Testing factory")
+    base: Creature = factory.create_base()
+    evolved = factory.create_evolved()
+
+    print(base.describe())
+    print(base.attack())
+    print(evolved.describe())
+    print(evolved.attack())
+    print()
+
+
+def battle(fact1, fact2):
+    print("Testing battle")
+    c1 = fact1.create_base()
+    c2 = fact2.create_base()
+
+    print(c1.describe())
+    print(" vs.")
+    print(c2.describe())
+    print(" fight!")
+    print(c1.attack())
+    print(c2.attack())
+
+
+def main():
     flame_factory = ex0.FlameFactory()
-    flameling = flame_factory.create_base()
-    pyrodon = flame_factory.create_evolved()
-    print(flameling.describe())
-    print(pyrodon.describe())
+    aqua_factory = ex0.AquaFactory()
+
+    test_factory(flame_factory)
+    test_factory(aqua_factory)
+    battle(flame_factory, aqua_factory)
 
 
 if __name__ == "__main__":
